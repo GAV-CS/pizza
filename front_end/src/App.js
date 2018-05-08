71430f2fccdaaf4e3fdb9330980f83342f6d6427
DrD0leful <daniel@danlife.com>
2018-05-08 14:14:04 -0700

    Added a camera button

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Button } from 'reactstrap';
 import logo from './logo.svg';
+import camera from './camera.svg';
 import './App.css';
 
 class App extends Component {
@@ -11,11 +13,15 @@ class App extends Component {
           <h1 className="App-title">Gavilan Computer Science Club Pizza or Not Pizza App</h1>
         </header>
         <p className="App-intro">
-          First iteration of the Front End portion. Code is found at: <code>front_end/src/App.js</code>. 
+          First iteration of the Front End portion. Code is found at: <code>front_end/src/App.js</code>.  
+        </p>
+        <p>
+        <input type="image" className="myImage" id="Use this for Cloud Vision API" src={camera}/>
         </p>
       </div>
     );
   }
 }
+
 
 export default App;
